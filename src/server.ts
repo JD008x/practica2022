0908fee7518de8a377de1a769e5c847164f1a2c0
Andrei <andreidei0406@yahoo.com>
2022-09-08 12:27:03 +0300

    -created location class -you can now go to a specific route and get a location -linked class with mongodb -TO DO: link location class with the attribute from InventoryItem class

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ const app = express();
 import mongoose from 'mongoose';
 import {env} from './env';
 import {inventoryItemRouter} from './routes/inventoryItem.routes';
+import {inventoryLocationRouter} from './routes/inventoryLocation.routes';
 
 connectToDatabase().catch(err => console.log(err));
 
@@ -16,5 +17,5 @@ app.use(express.urlencoded({ extended: false }));
     app.use(express.json());
 
 app.use('/inventory', inventoryItemRouter);
-
+app.use('/location', inventoryLocationRouter);
 app.listen(env.PORT, () => console.log('Server started on PORT '+env.PORT));
---
+++ src/services/inventoryLocation.service.ts
@@ -0,0 +1,26 @@
+import { InventoryLocation } from "../models/inventoryLocation.model";
+import { InventoryLocationDB } from "../schemas/inventoryLocation.schema";
+
+export async function postLocation(
+    inventoryLocation: InventoryLocation
+): Promise<Error | InventoryLocation>{
+    if(!inventoryLocation || !inventoryLocation.locationName){
+        return Error("The parameters given are not valid!");
+    }
+    try {
+        const exists = await InventoryLocationDB.findOne({ user: inventoryLocation.locationName });
+        if (exists) {
+          return Error("The item added to the database already exists!");
+        }
+      } catch (ex: any) {
+        return ex;
+      }
+    const NewInventoryLocation = new InventoryLocationDB({
+        locationName: inventoryLocation.locationName,
+        address: inventoryLocation.address,
+        managerName: inventoryLocation.managerName,
+        phoneNumber: inventoryLocation.phoneNumber
+    })
+    NewInventoryLocation.save();
+    return NewInventoryLocation;
+}
