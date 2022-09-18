db13418e0f4034a2c3141ed17b215aff00ab6471
Andrei Oprea <andreidei0406@yahoo.com>
2022-09-18 10:06:33 +0300

    -finished the scan page

--- frontend/src/app/menu-items/scan/scan.component.ts
+++ frontend/src/app/menu-items/scan/scan.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ZXingScannerModule} from '@zxing/ngx-scanner'
+import {BarcodeFormat} from '@zxing/library'
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'scan',
@@ -8,9 +11,32 @@ import {ZXingScannerModule} from '@zxing/ngx-scanner'
 })
 export class ScanComponent implements OnInit {
 
-  constructor() { }
+  scannerFormats = [BarcodeFormat.QR_CODE];
+  currentDevice!: MediaDeviceInfo;
+  availableDevices!: MediaDeviceInfo[];
+  hasDevices!:boolean;
+  hasPermission!:boolean;
+  torchEnabled = false;
+  tryHarder = false;
+  allowEmptyString =  true;
+
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+
+  onCamerasFound(devices:MediaDeviceInfo[]):void{
+    this.availableDevices = devices;
+    this.hasDevices = Boolean(devices && devices.length)
+  }
+
+  onCodeResult(resultString:string){
+    this.router.navigate(['/item/' + resultString]);
+  }
+
+  onHasPermission(has:boolean){
+    this.hasPermission = true;
   }
 
 }
