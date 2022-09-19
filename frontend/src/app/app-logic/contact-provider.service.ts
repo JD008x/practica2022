e3c7338dcf6e528137b097f60315b69b76c30333
AlexandraCucu <cucu_valeria@yahoo.com>
2022-09-19 14:52:53 +0300

    create contact

---
+++ frontend/src/app/app-logic/contact-provider.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {ContactData} from './contact-data';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactProviderService {
+  providedData = <ContactData>{
+    info: 'Inventory application',
+    address: 'Str. Turnului nr. 5',
+    openDays: 'Luni-Vineri',
+    timeSlot: '9:00 - 17:00',
+    phone: '+40747432345'
+  }
+
+  constructor() { }
+}
