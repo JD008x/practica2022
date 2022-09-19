f8ae81cab089c3e8fb802ef9ac9917817658c696
Andrei <andreidei0406@yahoo.com>
2022-09-19 14:37:20 +0300

    -added text on homepage -created scanner component -TO DO: CONNECTION WITH DB

--- frontend/src/app/menu-items/scan/scan.component.ts
+++ frontend/src/app/menu-items/scan/scan.component.ts
@@ -1,8 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import {ZXingScannerModule} from '@zxing/ngx-scanner'
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 import {BarcodeFormat} from '@zxing/library'
 import { Router } from '@angular/router';
 
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
 
 @Component({
   selector: 'scan',
