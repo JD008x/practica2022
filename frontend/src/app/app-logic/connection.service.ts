f43bdb4a936c75d8d93666a6af9992032b872283
Andrei <andreidei0406@yahoo.com>
2022-09-21 18:25:34 +0300

    -added every service into one file

--- frontend/src/app/app-logic/connection.service.ts
+++ frontend/src/app/app-logic/connection.service.ts
@@ -1,54 +1,71 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { InventoryItem } from '../../../../backend/src/models/inventoryItem.model';
 import { Observable } from 'rxjs';
 import { ObjectId } from 'mongoose';
 import { InventoryItemDB } from '../../../../backend/src/schemas/inventoryItem.schema';
-import {postInventory} from '../../../../backend/src/services/inventoryItem.service'
+import { postInventory } from '../../../../backend/src/services/inventoryItem.service';
 import { User } from '../../../../backend/src/models/user.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ConnectionService {
-  constructor(private http: HttpClient) {}
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: 'my-auth-token',
+    }),
+  };
+  inventoryUrl = 'http://localhost:3000/inventory';
+  userUrl = 'http://localhost:3000/user';
+  inventoryData: Array<InventoryItem>;
 
-  inventoryData !: Array<InventoryItem>;
-
-  getDataFromBackend(): Observable<InventoryItem[]>{
-    return this.http.get<InventoryItem[]>('http://localhost:3000/inventory');
+  constructor(private http: HttpClient) {
+    this.inventoryData = new Array<InventoryItem>();
   }
 
-  getUsersFromBackend(): Observable<User[]>{
-    return this.http.get<User[]>('http://localhost:3000/user');
+  getDataFromBackend(): Observable<InventoryItem[]> {
+    return this.http.get<InventoryItem[]>(this.inventoryUrl);
   }
-  
-  getItemById(id:ObjectId):InventoryItem{
-    this.getDataFromBackend().subscribe(result => {
-      if(!result){
-        return ;
+
+  getUsersFromBackend(): Observable<User[]> {
+    return this.http.get<User[]>(this.userUrl);
+  }
+
+  // getItemById(id: ObjectId) {
+  //   return this.http.get<InventoryItem>(this.inventoryUrl + String(id));
+  // }
+  getItemById(id: ObjectId): InventoryItem {
+    this.getDataFromBackend().subscribe((result) => {
+      if (!result) {
+        return;
       }
       this.inventoryData = result;
-     })
+    });
     return this.inventoryData.filter((x) => x._id == id)[0];
-
   }
 
-  addItem(item:InventoryItem){
-    console.log(item);
-    // console.log(InventoryItemDB.db.name);
-    // const NewInventoryItem = new InventoryItemDB({
-    //   user : item.user,
-    //   name: item.name ,
-    //   category: item.category ,
-    //   inventoryNumber : item.inventoryNumber,
-    //   addedDate: item.addedDate,
-    //   modifiedDate: item.modifiedDate ,
-    //   location: item.location,
-    //   isDeleted: item.isDeleted
-    // });
-
-    // NewInventoryItem.save();
+  verifyExistance(inventoryNumber: number): boolean {
+    this.getDataFromBackend().subscribe((result) => {
+      if (!result) {
+        return;
+      }
+      this.inventoryData = result;
+    });
+    if (
+      this.inventoryData.filter((x) => x.inventoryNumber == inventoryNumber)[0]
+    ) {
+      return true;
+    }
+    return false;
   }
 
+  addItem(item: InventoryItem): Observable<InventoryItem> {
+    return this.http.post<InventoryItem>(
+      this.inventoryUrl,
+      item,
+      this.httpOptions
+    );
+  }
 }
--- frontend/src/app/menu-items/inventory/inventory.component.ts
+++ frontend/src/app/menu-items/inventory/inventory.component.ts
@@ -4,18 +4,16 @@ import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
 import { SelectionModel } from '@angular/cdk/collections';
 import { ConnectionService } from 'src/app/app-logic/connection.service';
-import { InventoryItem } from '../../../../../backend/src/models/inventoryItem.model'
-import { Observable, tap } from 'rxjs';
-import { InventoryService } from 'src/app/app-logic/inventory.service';
 
 @Component({
   selector: 'inventory',
   templateUrl: './inventory.component.html',
-  styleUrls: ['./inventory.component.css']
+  styleUrls: ['./inventory.component.css'],
 })
 export class InventoryComponent implements OnInit {
-
-  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator | undefined;
+  @ViewChild(MatPaginator, { static: true }) paginator:
+    | MatPaginator
+    | undefined;
   @ViewChild(MatSort, { static: true }) sort: MatSort | undefined;
 
   inventoryItems: any;
@@ -31,23 +29,23 @@ export class InventoryComponent implements OnInit {
     'addedDate',
     'modifiedDate',
     'isDeleted',
-    'actions'
+    'actions',
   ];
   selection = new SelectionModel<Element>(true, []);
 
-  constructor(private inventoryList: InventoryService) {
+  constructor(private inventoryList: ConnectionService) {
     this.inventoryItems = inventoryList.getDataFromBackend();
-   }
+  }
 
   ngOnInit(): void {
-    this.inventoryList.getDataFromBackend().subscribe(result => {
-      if(!result){
-        return ;
+    this.inventoryList.getDataFromBackend().subscribe((result) => {
+      if (!result) {
+        return;
       }
-      this.inventoryItems= new MatTableDataSource(result);
-      this.inventoryItems.sort= this.sort;
-      this.inventoryItems.paginator=this.paginator;
-     })
+      this.inventoryItems = new MatTableDataSource(result);
+      this.inventoryItems.sort = this.sort;
+      this.inventoryItems.paginator = this.paginator;
+    });
   }
 
   isAllSelected() {
@@ -57,7 +55,10 @@ export class InventoryComponent implements OnInit {
   }
 
   masterToggle() {
-    this.isAllSelected() ? this.selection.clear() :
-      this.inventoryItems.data.forEach((row: Element) => this.selection.select(row));
+    this.isAllSelected()
+      ? this.selection.clear()
+      : this.inventoryItems.data.forEach((row: Element) =>
+          this.selection.select(row)
+        );
   }
 }
