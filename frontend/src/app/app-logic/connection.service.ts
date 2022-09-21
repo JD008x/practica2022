511cd68d465444dbb6aa79362da98f3b99487586
Andrei <andreidei0406@yahoo.com>
2022-09-21 16:30:38 +0300

    -formatted code

--- frontend/src/app/app-logic/connection.service.ts
+++ frontend/src/app/app-logic/connection.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { InventoryItem } from '../../../../backend/src/models/inventoryItem.model';
-import { Observable, tap } from 'rxjs';
-
+import { Observable } from 'rxjs';
+import { ObjectId } from 'mongoose';
+import { InventoryItemDB } from '../../../../backend/src/schemas/inventoryItem.schema';
+import {postInventory} from '../../../../backend/src/services/inventoryItem.service'
 import { User } from '../../../../backend/src/models/user.model';
 
 @Injectable({
@@ -10,6 +12,8 @@ import { User } from '../../../../backend/src/models/user.model';
 })
 export class ConnectionService {
   constructor(private http: HttpClient) {}
+
+  inventoryData !: Array<InventoryItem>;
 
   getDataFromBackend(): Observable<InventoryItem[]>{
     return this.http.get<InventoryItem[]>('http://localhost:3000/inventory');
@@ -19,4 +23,32 @@ export class ConnectionService {
     return this.http.get<User[]>('http://localhost:3000/user');
   }
   
+  getItemById(id:ObjectId):InventoryItem{
+    this.getDataFromBackend().subscribe(result => {
+      if(!result){
+        return ;
+      }
+      this.inventoryData = result;
+     })
+    return this.inventoryData.filter((x) => x._id == id)[0];
+
+  }
+
+  addItem(item:InventoryItem){
+    console.log(item);
+    // console.log(InventoryItemDB.db.name);
+    // const NewInventoryItem = new InventoryItemDB({
+    //   user : item.user,
+    //   name: item.name ,
+    //   category: item.category ,
+    //   inventoryNumber : item.inventoryNumber,
+    //   addedDate: item.addedDate,
+    //   modifiedDate: item.modifiedDate ,
+    //   location: item.location,
+    //   isDeleted: item.isDeleted
+    // });
+
+    // NewInventoryItem.save();
+  }
+
 }
